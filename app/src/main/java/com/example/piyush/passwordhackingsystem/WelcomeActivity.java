0da739f7b5847db1fbe6fc43400289dfdd2b347a
Abhishek Yadav <abhishekgit1208@gmail.com>
2016-10-11 22:11:04 +0530

    Final changes

--- app/src/main/java/com/example/piyush/passwordhackingsystem/WelcomeActivity.java
+++ app/src/main/java/com/example/piyush/passwordhackingsystem/WelcomeActivity.java
@@ -3,16 +3,33 @@ package com.example.piyush.passwordhackingsystem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Handler;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    TextView textView;
+    RelativeLayout relativeLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        relativeLayout = (RelativeLayout) findViewById(R.id.activity_welcome);
+        textView = (TextView) findViewById(R.id.textView6);
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(WelcomeActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
          android.os.Handler handler;
         Runnable delayRunnable;
         handler=new android.os.Handler();
